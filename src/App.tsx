57715d723e5378d46d0f77fe966274f40578e963
hadi <hadialqahtani15@gmail.com>
2023-11-24 13:04:27 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <div className='div'>
+      <h1>The comment💀</h1>
+      <img className='comment' src="src\assets\comment.PNG" alt="comment" />
+      <a className='link' href="https://www.tiktok.com/@haadi.aep">
+        <h1>My TikTok acc</h1>
+      </a>
+    </div>
+  )
+}
